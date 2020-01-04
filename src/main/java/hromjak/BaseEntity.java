b454dcaf883cbd39aa5c5483f970dcdf7dfce36f
Hromjak Robert <mortrum6@gmail.com>
2020-01-04 18:10:51 +0200

    Adding new class BaseEntuty

---
+++ src/main/java/hromjak/BaseEntity.java
@@ -0,0 +1,21 @@
+package hromjak;
+
+public class BaseEntity {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
